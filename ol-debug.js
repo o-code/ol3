88869dc3ea4755c5b165eaa44208698cc8d32a33
Павел Гайдаржи <pavel.gdr@gmail.com>
2016-07-20 11:09:50 +0400

    added ol.module code right into ol-debug.js
    added ol.module code right into ol-debug.js

--- ol-debug.js
+++ ol-debug.js
@@ -107539,3 +107539,31 @@ OPENLAYERS.ol = ol;
 
   return OPENLAYERS.ol;
 }));
+
+
+/*OpenLayers wrapper*/
+angular.module('OpenLayers',[]).factory('ol', [
+    '$window',
+    function ($window) {
+        if($window.ol){
+            //Delete from window so it's not globally accessible, but keep in _thirdParty for testing
+            $window._thirdParty = $window._thirdParty || {};
+            $window._thirdParty.ol = $window.ol;
+
+            //OpenLayers fails without global 'ol' :(
+            //try {
+            //    delete $window.ol;
+            //} catch (e) {
+            //    /*<IE8 doesn't do delete of window vars, make undefined if delete error*/
+            //    $window.ol = undefined;
+            //}
+        }
+        var ol = $window._thirdParty.ol;
+        return ol;
+    }
+]).factory('WKT',[
+    'ol',
+    function(ol){
+        return new ol.format.WKT();
+    }
+]);
